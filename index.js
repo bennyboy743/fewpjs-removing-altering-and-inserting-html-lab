646ada374535505765cefa5594ca395a915a88c2
Benjamin <benjaminrowe0@gmail.com>
2022-01-13 08:32:00 +1100

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const removingMain = document.getElementById('main');
+removingMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ben is the champion';
+
+document.body.append(newHeader)
